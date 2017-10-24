4667ad68fcde75521077fdcdf7a1e289a4d168b6
andreea penu <andreea_penu@hotmail.com>
2017-10-24 18:01:19 +0200

    seeder articles empty

--- project-md/database/seeds/ArticlesTableSeeder.php
+++ project-md/database/seeds/ArticlesTableSeeder.php
@@ -13,34 +13,5 @@ class ArticlesTableSeeder extends Seeder
     {
         //
 
-
-        DB::table('articles')->delete();
-
-        DB::table('articles')->insert(array(
-
-
-            array('title'=>'wortel',
-                'text'=>'Ik heb 5 wortels en kan ze niet meer op krijgen',
-                'datum'=>'2017-10-25',
-           
-                'pic'=>'wortel.jpg',
-                'tijdstip'=>'14:00 - 17:00',
-                'user_id'=>'1'),
-            array('title'=>'zoeteaardappelen',
-                'text'=>'Deze aardappelen vorige week aangekocht, maar kan er niks meer mee doen',
-                'datum'=>'2017-10-24',
-              
-                'pic'=>'zoeteaardappel.jpg',
-                'tijdstip'=>'17:00 - 19:00',
-                'user_id'=>'2'),
-            array('title'=>'Advocados',
-                'text'=>'Gekocht om te proeven, maar vind ze toch niet lekker.',
-                'datum'=>'2017-10-24',
-                'pic'=>'avocado.jpg',
-                'tijdstip'=>'19:00 - 20:00',
-                'user_id'=>'3')
-
-        ));
-
     }
 }
